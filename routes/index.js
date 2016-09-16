a910a3f1aed97896b7f2d48837c230c7b3ea4f2e
Tanya Powell <hello@tanyapowell.co.uk>
2016-09-16 23:07:24 +0100

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+var db = require('./../models/schema');
+
+router.get('/', function (req, res) {
+  res.render('index', { title: 'Hey'});
+});
+
+router.get('/api/puppies', db.getAllPuppies);
+router.get('/api/puppies/:id', db.getSinglePuppy);
+router.post('/api/puppies', db.createPuppy);
+router.put('/api/puppies/:id', db.updatePuppy);
+router.delete('/api/puppies/:id', db.removePuppy);
+
+module.exports = router;
