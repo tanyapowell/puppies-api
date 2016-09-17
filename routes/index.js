68bf0e8ce2bd1e6fee2d415dea0383a315152f86
Tanya Powell <hello@tanyapowell.co.uk>
2016-09-17 10:34:54 +0100

    hmmm

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var db = require('./../models/queries');
+var db = require('../models/schema');
 
 router.get('/', function (req, res) {
   res.render('index', { title: 'Hey Hey'});
